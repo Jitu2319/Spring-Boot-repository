972f64c26441f7215a17cf90578d20dfe7a31f62
Jitu2319 <demojitendra@gmail.com>
2022-09-02 12:01:48 +0530

    Test

---
+++ TestProjectGithub/src/main/java/Generate_Pdf/TestProjectGithubApplication.java
@@ -0,0 +1,13 @@
+package Generate_Pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectGithubApplication.class, args);
+	}
+
+}
